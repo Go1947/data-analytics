92c6873aba672b4c3ee36e6d3d70af48b9b0001a
Go1947 <govind2912000@gmail.com>
2023-09-25 22:55:27 +0530

    python module Q4

---
+++ Q4.py
@@ -0,0 +1,4 @@
+text = input("Please input a list of words to evaluate: ")
+print ("The original string is : " + text)
+res = len(text.split())
+print ("The number of words in string are : " + str(res))
