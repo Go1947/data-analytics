58d9b93bfd6ca788eabda280e47e5ada685ec486
Go1947 <govind2912000@gmail.com>
2023-10-03 15:50:38 +0530

    python module list Q1

---
+++ listQ1.py
@@ -0,0 +1,7 @@
+#python list
+#Q.1 write a program to check a list is empty or not
+list1 = ['ase','ahsbjw','AJSNBWH']
+if len(list1) == 0:
+    print("Empty List")
+else:
+    print("The list is not empty") 
